d20f4ad1eff2e6ec28017bd7371a478a801bc329
Itai Lifshitz <itailif34@gmail.com>
2025-06-02 19:43:02 +0300

    fixing battleinfo flow1 02.06.2025

--- common/TankAlgorithm.h
+++ common/TankAlgorithm.h
@@ -8,8 +8,9 @@ class TankAlgorithm {
 private:
     std::shared_ptr<Tank> my_tank; // Pointer to the tank controlled by this algorithm
     std::shared_ptr<GameBoard> game_board; // Pointer to the game board
-    std::vector<std::shared_ptr<Tank>> opponents; // List of opponent tanks
+    //std::vector<std::shared_ptr<Tank>> opponents; // List of opponent tanks
     bool have_battle_info = false;
+    std::vector<std::pair<size_t, size_t>> enemy_locations;
 public:
     virtual ~TankAlgorithm() {} // Virtual destructor for proper cleanup of derived classes
     virtual ActionRequest getAction() = 0;
